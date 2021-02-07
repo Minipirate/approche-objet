46f9fbc916e84a68cf9f89942a02dac837a0fc9c
git <nessti@live.fr>
2021-02-07 20:09:11 +0100

    TP J3 & J4 -> Exercice ManipulationChaine

---
+++ approche-objet/src/fr/diginamic/entites/Salarie.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package fr.diginamic.entites;
+
+/**
+ * @author Ness'ti
+ *
+ */
+public class Salarie {
+
+	private String nom;
+	private String prenom;
+	private double salaire;
+
+	public Salarie(String nom, String prenom, double salaire) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.salaire = salaire;
+	}
+
+	@Override
+	public String toString() {
+		return "Salarie [nom=" + nom + ", prenom=" + prenom + ", salaire=" + salaire + "]";
+	}
+}
